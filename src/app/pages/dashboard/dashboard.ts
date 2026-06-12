1fab79a907c418a0ca5d165b9e6bef1101861f29
Hajar ARHOUM <bilalteffal3@gmail.com>
2026-06-12 22:18:19 +0100

    Correction affichage dashboard premier chargement

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 import { SidebarComponent } from '../../layout/sidebar/sidebar';
 import { DashboardService, DashboardStats } from '../../services/dashboard';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, DecimalPipe],
   templateUrl: './dashboard.html',
   styleUrls: ['./dashboard.css']
 })
@@ -13,12 +14,23 @@ export class DashboardComponent implements OnInit {
 
   stats?: DashboardStats;
 
-  constructor(private dashboardService: DashboardService) {}
+  constructor(
+    private dashboardService: DashboardService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.chargerStats();
+    }, 100);
+  }
+
+  chargerStats(): void {
     this.dashboardService.getStats().subscribe({
       next: (data) => {
+        console.log('STATS DASHBOARD = ', data);
         this.stats = data;
+        this.cd.detectChanges();
       },
       error: (err) => {
         console.log('Erreur dashboard stats', err);
